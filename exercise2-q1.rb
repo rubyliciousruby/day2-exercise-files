5b3b599cf48bd784c504e20a137146b387b49d0e
Ruby Li <rubyli@Ruby-Lis-MacBook-Pro-3.local>
2015-08-19 06:56:37 -0400

    Day 2 Exercise 2 Part 1

--- exercise2-q1.rb
+++ exercise2-q1.rb
@@ -1,6 +1,6 @@
 #Set a variable meal equal 55 
 meal = 55
 
-#good tip for a 55 dollar meal is 15% of the total meal cost.
+#A good tip for a 55 dollar meal is 15% of the total meal cost.
 tip = meal * 0.15
 puts "A good tip for a 55 dollar meal is #{tip} dollars."
